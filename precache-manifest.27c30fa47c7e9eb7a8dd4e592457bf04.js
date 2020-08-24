c95afb1a9f95fdabce13e0d36c3a50dd4d211d16
nada <nada@internal.eplango.com>
2020-08-24 11:07:51 +0200

    Updates

---
+++ precache-manifest.27c30fa47c7e9eb7a8dd4e592457bf04.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6083699e0b593ef1433d5ee4290cf79",
+    "url": "/travelicious/index.html"
+  },
+  {
+    "revision": "cf0438d223dfbe2a74f6",
+    "url": "/travelicious/static/css/2.0cbfc84d.chunk.css"
+  },
+  {
+    "revision": "53e596896476181d61fc",
+    "url": "/travelicious/static/css/main.3ec8b25b.chunk.css"
+  },
+  {
+    "revision": "cf0438d223dfbe2a74f6",
+    "url": "/travelicious/static/js/2.493759d1.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/travelicious/static/js/2.493759d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e596896476181d61fc",
+    "url": "/travelicious/static/js/main.29bd207a.chunk.js"
+  },
+  {
+    "revision": "1c84643521368e3a9d85",
+    "url": "/travelicious/static/js/runtime-main.cdf825c2.js"
+  },
+  {
+    "revision": "1a922012367a68d25beb16f5dc613ac5",
+    "url": "/travelicious/static/media/Travelicious-logo.1a922012.png"
+  },
+  {
+    "revision": "4e31d3a363723298cbbd9de6ffd0b70f",
+    "url": "/travelicious/static/media/bgn-heading-06.4e31d3a3.jpg"
+  },
+  {
+    "revision": "03fea2ee0c688e5d4e687e1a52554050",
+    "url": "/travelicious/static/media/bgn-home-slider-03.03fea2ee.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/travelicious/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/travelicious/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/travelicious/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/travelicious/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/travelicious/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/travelicious/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/travelicious/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/travelicious/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/travelicious/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/travelicious/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/travelicious/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/travelicious/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/travelicious/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/travelicious/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/travelicious/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "6f79c2158035cb34ce54e2b265ee9949",
+    "url": "/travelicious/static/media/google map .6f79c215.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/travelicious/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/travelicious/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/travelicious/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/travelicious/static/media/slick.f97e3bbf.svg"
+  }
+]);
